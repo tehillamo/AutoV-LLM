6bea74980c03b0b43175305aadac68cf57404a94
Paul <paulungermann@web.de>
2023-10-27 12:05:55 +0200

    fix

--- data_pipeline/embeddings.py
+++ data_pipeline/embeddings.py
@@ -2,6 +2,7 @@ from sentence_transformers import SentenceTransformer
 import pandas as pd 
 from sklearn.decomposition import PCA
 import numpy as np
+from sklearn.manifold import TSNE
 
 class Embeddings:
 
@@ -17,6 +18,8 @@ class Embeddings:
             return self._pca(embeddings, f"{column}_embedding", dimension)
         if reduction_algorithm == "TSNE":
             return self._tsne(embeddings, f"{column}_embedding", dimension)
+        if reduction_algorithm == "both":
+            return self._both(embeddings, f"{column}_embedding", dimension)
 
     def _embedding(self, df, column, model):
         df[f"{column}_embedding"] = df.apply(lambda row: self._get_embedding(row, column, model), axis=1)
@@ -30,12 +33,31 @@ class Embeddings:
         pca = PCA(n_components=dimension)
         vals = np.asarray([np.array(x) for x in df[column].values])
         pca.fit(vals)
-        df[f"{column}_reduced"] = df.apply(lambda row: pca.transform(np.asarray(row[column].reshape(1, -1)))[0], axis=1)
+        df[f"{column}_reduced_pca"] = df.apply(lambda row: pca.transform(np.asarray(row[column].reshape(1, -1)))[0], axis=1)
         return df
 
 
     def _tsne(self, df, column, dimension):
-        pass
+        vals = np.asarray([np.array(x) for x in df[column].values])
+        tsne = TSNE(n_components=dimension, learning_rate='auto', init='random', perplexity=5)
+        transformed = tsne.fit_transform(vals)
+        transformed = [np.asarray(x) for x in transformed]
+        print(transformed)
+        #df[f"{column}_reduced_tsne"] = np.asarray(transformed)
+        df[f"{column}_reduced_tsne"] = transformed
+        return df
+    
+    def _both(self,df ,column, dimension):
+        n_comp = min(dimension, df.shape[0], df.shape[1])
+        pca = PCA(n_components=n_comp)
+        vals = np.asarray([np.array(x) for x in df[column].values])
+        transformed_pca = pca.fit_transform(vals)
+        tsne = TSNE(n_components=dimension, learning_rate='auto', init='random', perplexity=5)
+        transformed_tsne = tsne.fit_transform(transformed_pca)
+        transformed_tsne = [np.asarray(x) for x in transformed_tsne]
+        df[f"{column}_reduced_both"] = transformed_tsne
+        return df
+        
 
     
 
--- data_pipeline/scripts.py
+++ data_pipeline/scripts.py
@@ -2,25 +2,21 @@ import argparse
 import os
 import speech2text
 import embeddings
-
-""" s2text = speech2text.Speech2Text("../ressources", "base")
-df = s2text.transcribe()
-create_embeddings = embeddings.Embeddings()
-df = create_embeddings.create_embeddings(df, "transcribed_text")
-df.to_csv("../output/output.csv", sep=';', index=False, columns=["uuid", "trial_number", "transcribed_text", "transcribed_text_embedding_reduced"]) """
+import random
 
 def main():
+    random.seed(543547)
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", help="Path to the ressources folder.", type=str, required=True)
     parser.add_argument("--output", help="Path to the output file.", type=str, required=True)
-    parser.add_argument("--reduction_algorithm", help="Reduction algorithm to use.",choices=["PCA", "TSNE"], type=str, default = "PCA")
+    parser.add_argument("--reduction_algorithm", help="Reduction algorithm to use.",choices=["PCA", "TSNE", "both"], type=str, default = "PCA")
     parser.add_argument("--dimension", type=int, help="Dimension to reduce to.", default = 2)
     args = parser.parse_args()
     s2text = speech2text.Speech2Text(args.path, "base")
     df = s2text.transcribe()
     create_embeddings = embeddings.Embeddings()
     df = create_embeddings.create_embeddings(df, "transcribed_text", dimension=args.dimension, reduction_algorithm=args.reduction_algorithm)
-    df.to_csv(os.path.join(args.output, "output.csv"), sep=';', index=False, columns=["uuid", "trial_number", "transcribed_text", "transcribed_text_embedding_reduced"])
+    df.to_csv(os.path.join(args.output, "output.csv"), sep=';', index=False, columns=["uuid", "trial_number", "transcribed_text", "transcribed_text_embedding_reduced_pca"])
 
 if __name__ == "__main__":
     main()
